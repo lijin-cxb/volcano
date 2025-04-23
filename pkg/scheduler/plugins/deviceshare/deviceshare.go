debb4dff60df5074f586e5fbab45d5886ecf21b2
lijin-cxb <76936727+lijin-cxb@users.noreply.github.com>
2025-04-23 16:18:51 +0800

    deviceshare plugin return nodescore

--- pkg/scheduler/plugins/deviceshare/deviceshare.go
+++ pkg/scheduler/plugins/deviceshare/deviceshare.go
@@ -163,6 +163,7 @@ func (dp *deviceSharePlugin) OnSessionOpen(ssn *framework.Session) {
 			// TODO: we should use a seperate plugin for devices, and seperate them from predicates and nodeOrder plugin.
 			nodeScore := float64(score) * float64(dp.scheduleWeight)
 			klog.V(5).Infof("Node: %s, task<%s/%s> Device Score weight %d, score: %f", node.Name, task.Namespace, task.Name, dp.scheduleWeight, nodeScore)
+		        return nodeScore, nil
 		}
 		return 0, nil
 	})
